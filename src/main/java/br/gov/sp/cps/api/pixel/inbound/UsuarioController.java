985e8dc3fa9cc45bba91bf401086d5b42645506c
wdeus <wagner.silva96@fatec.sp.gov.br>
2025-04-12 15:15:51 -0300

    feat(PX-78): Implementa endpoint para deleção de usuario

--- src/main/java/br/gov/sp/cps/api/pixel/inbound/UsuarioController.java
+++ src/main/java/br/gov/sp/cps/api/pixel/inbound/UsuarioController.java
@@ -3,14 +3,13 @@ package br.gov.sp.cps.api.pixel.inbound;
 import br.gov.sp.cps.api.pixel.core.domain.dto.PlantacaoDTO;
 import br.gov.sp.cps.api.pixel.core.domain.dto.command.CadastrarPlantacaoCommand;
 import br.gov.sp.cps.api.pixel.core.domain.dto.command.CadastrarUsuarioCommand;
+import br.gov.sp.cps.api.pixel.core.domain.dto.command.DeletarUsuarioCommand;
 import br.gov.sp.cps.api.pixel.core.domain.entity.Usuario;
 import br.gov.sp.cps.api.pixel.core.usecase.CadastrarUsuarioUC;
+import br.gov.sp.cps.api.pixel.core.usecase.DeletarUsuarioUC;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
@@ -21,6 +20,7 @@ import java.net.URI;
 public class UsuarioController {
 
     private final CadastrarUsuarioUC cadastrarUsuarioUC;
+    private final DeletarUsuarioUC deletarUsuarioUC;
 
     @PostMapping
     public ResponseEntity<Usuario> cadastrarUsuario(@RequestBody CadastrarUsuarioCommand command) throws Exception {
@@ -30,5 +30,11 @@ public class UsuarioController {
                 .buildAndExpand(usuario.getId())
                 .toUri();
         return ResponseEntity.created(location).body(usuario);
+    }
+
+    @DeleteMapping("/{idUsuario}")
+    public ResponseEntity<Usuario> deletarUsuario(@PathVariable Long idUsuario) throws Exception {
+        deletarUsuarioUC.executar(new DeletarUsuarioCommand(idUsuario));
+        return ResponseEntity.ok().build();
     }
 }
