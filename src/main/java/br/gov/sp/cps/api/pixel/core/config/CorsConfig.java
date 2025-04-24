9a1e492d0d6bb14940a9df36d135b0399e479cc9
xxzidanilloxx <wesleydanillo.silva@gmail.com>
2025-04-23 22:28:17 -0300

    fix(PX-108): corrige erro de CORS

--- src/main/java/br/gov/sp/cps/api/pixel/core/config/CorsConfig.java
+++ src/main/java/br/gov/sp/cps/api/pixel/core/config/CorsConfig.java
@@ -1,24 +1,37 @@
 package br.gov.sp.cps.api.pixel.core.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.lang.NonNull;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
 
-@Configuration
-public class CorsConfig {
+import java.io.IOException;
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(@NonNull CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("*")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*");
-            }
-        };
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class CorsConfig implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        final HttpServletResponse response = (HttpServletResponse) res;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+        response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, enctype");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Expose-Headers", "Authorization");
+        response.setHeader("Access-Control-Max-Age", "3600");
+
+        if ("OPTIONS".equalsIgnoreCase(((HttpServletRequest) req).getMethod()))
+            response.setStatus(HttpServletResponse.SC_OK);
+        else
+            chain.doFilter(req, res);
     }
+
+    @Override
+    public void destroy() { }
+
+    @Override
+    public void init(FilterConfig arg0) throws ServletException { }
 }
