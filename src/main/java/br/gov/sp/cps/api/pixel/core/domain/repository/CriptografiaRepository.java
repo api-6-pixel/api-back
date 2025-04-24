0a7611a6b960c209d63ef7db049df9fca82691f7
Luciano Pamplona <102562662+lucianonps@users.noreply.github.com>
2025-04-23 22:09:26 -0300

    feat/PX-106: Criar controller

--- src/main/java/br/gov/sp/cps/api/pixel/core/domain/repository/CriptografiaRepository.java
+++ src/main/java/br/gov/sp/cps/api/pixel/core/domain/repository/CriptografiaRepository.java
@@ -1,5 +1,6 @@
 package br.gov.sp.cps.api.pixel.core.domain.repository;
 
+import br.gov.sp.cps.api.pixel.core.domain.dto.command.AlterarUsuarioCommand;
 import br.gov.sp.cps.api.pixel.core.domain.dto.command.CadastrarUsuarioCommand;
 import br.gov.sp.cps.api.pixel.core.domain.entity.Usuario;
 
@@ -11,5 +12,6 @@ public interface CriptografiaRepository {
     String encriptar(String plainText, SecretKey key) throws Exception;
     String descriptografar(String encryptedText, SecretKey key) throws Exception;
     Object getObjectEncriptografado(CadastrarUsuarioCommand command, SecretKey secretKey) throws Exception;
+    Object getObjectEncriptografado(AlterarUsuarioCommand command, SecretKey secretKey) throws Exception;
     Object getObjectDescriptografado(Usuario usuario, SecretKey secretKey) throws Exception;
 }
--- src/main/java/br/gov/sp/cps/api/pixel/inbound/UsuarioController.java
+++ src/main/java/br/gov/sp/cps/api/pixel/inbound/UsuarioController.java
@@ -2,10 +2,12 @@ package br.gov.sp.cps.api.pixel.inbound;
 
 import br.gov.sp.cps.api.pixel.core.domain.dto.PlantacaoDTO;
 import br.gov.sp.cps.api.pixel.core.domain.dto.UsuarioDTO;
+import br.gov.sp.cps.api.pixel.core.domain.dto.command.AlterarUsuarioCommand;
 import br.gov.sp.cps.api.pixel.core.domain.dto.command.CadastrarPlantacaoCommand;
 import br.gov.sp.cps.api.pixel.core.domain.dto.command.CadastrarUsuarioCommand;
 import br.gov.sp.cps.api.pixel.core.domain.dto.command.DeletarUsuarioCommand;
 import br.gov.sp.cps.api.pixel.core.domain.entity.Usuario;
+import br.gov.sp.cps.api.pixel.core.usecase.AlterarUsuarioUC;
 import br.gov.sp.cps.api.pixel.core.usecase.CadastrarUsuarioUC;
 import br.gov.sp.cps.api.pixel.core.usecase.CarregarUsuarioUC;
 import br.gov.sp.cps.api.pixel.core.usecase.DeletarUsuarioUC;
@@ -24,6 +26,7 @@ public class UsuarioController {
     private final CadastrarUsuarioUC cadastrarUsuarioUC;
     private final DeletarUsuarioUC deletarUsuarioUC;
     private final CarregarUsuarioUC carregarUsuarioUC;
+    private final AlterarUsuarioUC alterarUsuarioUC;
 
     @PostMapping
     public ResponseEntity<Usuario> cadastrarUsuario(@RequestBody CadastrarUsuarioCommand command) throws Exception {
@@ -38,6 +41,13 @@ public class UsuarioController {
     @GetMapping("/{idUsuario}")
     public ResponseEntity<UsuarioDTO>  obterUsuario(@PathVariable Long idUsuario) throws Exception {
         UsuarioDTO usuario = carregarUsuarioUC.executar(idUsuario);
+        return ResponseEntity.ok(usuario);
+    }
+
+    @PutMapping("/{idUsuario}")
+    public ResponseEntity<Usuario> alterarUsuario(@PathVariable Long idUsuario, @RequestBody AlterarUsuarioCommand command) throws Exception {
+        command.setId(idUsuario); // Garante que o ID da URL prevaleça
+        Usuario usuario = alterarUsuarioUC.executar(command);
         return ResponseEntity.ok(usuario);
     }
 
