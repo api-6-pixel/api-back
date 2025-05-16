c5bf55a7013645a973f5824d736a609eede1778d
xxzidanilloxx <wesleydanillo.silva@gmail.com>
2025-05-16 13:01:49 -0300

    feat(PX-150): Implementa a interface de repositório

--- src/main/java/br/gov/sp/cps/api/pixel/core/domain/repository/AtualizacaoPlantioRepository.java
+++ src/main/java/br/gov/sp/cps/api/pixel/core/domain/repository/AtualizacaoPlantioRepository.java
@@ -1,7 +1,12 @@
 package br.gov.sp.cps.api.pixel.core.domain.repository;
 
 import br.gov.sp.cps.api.pixel.core.domain.entity.AtualizacaoPlantio;
+import br.gov.sp.cps.api.pixel.core.domain.entity.Plantacao;
+
+import java.util.List;
+import java.util.stream.Stream;
 
 public interface AtualizacaoPlantioRepository {
     AtualizacaoPlantio salvar(AtualizacaoPlantio atualizacaoPlantio);
+    List<AtualizacaoPlantio> buscarPorPlantacao(Plantacao plantacao);
 }
--- src/main/java/br/gov/sp/cps/api/pixel/outbound/jpa/AtualizacaoPlantioJpaRepository.java
+++ src/main/java/br/gov/sp/cps/api/pixel/outbound/jpa/AtualizacaoPlantioJpaRepository.java
@@ -1,8 +1,11 @@
 package br.gov.sp.cps.api.pixel.outbound.jpa;
 
 import br.gov.sp.cps.api.pixel.core.domain.entity.AtualizacaoPlantio;
+import br.gov.sp.cps.api.pixel.core.domain.entity.Plantacao;
 import br.gov.sp.cps.api.pixel.core.domain.repository.AtualizacaoPlantioRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface AtualizacaoPlantioJpaRepository extends
         JpaRepository<AtualizacaoPlantio, Long>,
@@ -10,4 +13,10 @@ public interface AtualizacaoPlantioJpaRepository extends
     default AtualizacaoPlantio salvar(AtualizacaoPlantio atualizacaoPlantio) {
         return save(atualizacaoPlantio);
     }
+
+    default List<AtualizacaoPlantio> buscarPorPlantacao(Plantacao plantacao) {
+        return findByPlantacao(plantacao);
+    }
+
+    List<AtualizacaoPlantio> findByPlantacao(Plantacao plantacao);
 }
