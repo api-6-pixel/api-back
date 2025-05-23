67b31251cc5872af4646fd4204c850be3652e6e9
Matheus Fiebig <macrubig@gmail.com>
2025-05-23 00:58:18 -0300

    feat(PX-123): login para portabilidade

--- src/main/java/br/gov/sp/cps/api/pixel/core/service/ConfiguracaoSeguranca.java
+++ src/main/java/br/gov/sp/cps/api/pixel/core/service/ConfiguracaoSeguranca.java
@@ -32,9 +32,7 @@ public class ConfiguracaoSeguranca {
                         .requestMatchers(HttpMethod.POST, "/api/auth/login").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/historico/ativo").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/historico/aceite").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/api/portabilidade").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/api/portabilidade/**").permitAll()
-                        .requestMatchers(HttpMethod.PUT, "/api/portabilidade/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/portabilidade/auth").permitAll()
                         .anyRequest().authenticated())
                 .addFilterBefore(filtroSeguranca, UsernamePasswordAuthenticationFilter.class)
                 .build();
--- src/main/java/br/gov/sp/cps/api/pixel/inbound/PortabilidadeController.java
+++ src/main/java/br/gov/sp/cps/api/pixel/inbound/PortabilidadeController.java
@@ -1,6 +1,8 @@
 package br.gov.sp.cps.api.pixel.inbound;
 
+import br.gov.sp.cps.api.pixel.core.domain.dto.PortabilidadeLoginDTO;
 import br.gov.sp.cps.api.pixel.core.domain.dto.command.AutorizarPortabilidadeCommand;
+import br.gov.sp.cps.api.pixel.core.domain.dto.command.PortabilidadeLoginCommand;
 import br.gov.sp.cps.api.pixel.core.usecase.AutorizarPortabilidadeUC;
 import lombok.RequiredArgsConstructor;
 
@@ -13,6 +15,7 @@ import br.gov.sp.cps.api.pixel.core.domain.dto.UsuarioDTO;
 import br.gov.sp.cps.api.pixel.core.domain.dto.command.ObterUsuarioIDCommand;
 import br.gov.sp.cps.api.pixel.core.domain.dto.command.PortabilidadeCriarChaveCommand;
 import br.gov.sp.cps.api.pixel.core.usecase.CriarChavePortabilidadeUC;
+import br.gov.sp.cps.api.pixel.core.usecase.PortabilidadeLoginUC;
 import br.gov.sp.cps.api.pixel.core.usecase.ObterUsuarioUC;
 
 @RestController
@@ -23,6 +26,13 @@ public class PortabilidadeController {
     private final CriarChavePortabilidadeUC criarChavePortabilidadeUC;
     private final ObterUsuarioUC obterUsuarioUC;
     private final AutorizarPortabilidadeUC autorizarUsuarioUC;
+    private final PortabilidadeLoginUC loginUC;
+
+    @PostMapping("/auth")
+    public ResponseEntity<PortabilidadeLoginDTO> login(@RequestBody PortabilidadeLoginCommand command) throws Exception{
+        var chave = loginUC.executar(command);
+        return ResponseEntity.ok(chave);
+    }
 
     @PostMapping()
     public ResponseEntity<CriarChavePortabilidadeDTO> criarChavePortabilidade(@RequestBody PortabilidadeCriarChaveCommand command) throws Exception{
